db3228f213d91d8e258a4c2c40d787878aed620a
thomas-roth <uruox@student.kit.edu>
2025-03-24 23:31:25 +0100

    optimize vlm server config

--- iTRAP/models/Qwen2-VL/start_vlm_server.sh
+++ iTRAP/models/Qwen2-VL/start_vlm_server.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=3,4 python -m vllm.entrypoints.openai.api_server --model /home/troth/bt/iTRAP/iTRAP/models/Qwen2-VL/pretrained/qwen2_vl_lora_sft_calvin \
-                                                                    --host 0.0.0.0 --port 8000 --tensor-parallel-size 2 \
-                                                                    --gpu-memory-utilization 0.25 --max-model-len 25000 \
-                                                                    --served-model-name qwen2-vl
+CUDA_VISIBLE_DEVICES=2,3 TOKENIZERS_PARALLELISM=true python -m vllm.entrypoints.openai.api_server \
+ --model /home/troth/bt/iTRAP/iTRAP/models/Qwen2-VL/pretrained/qwen2_vl_lora_sft_calvin \
+ --host 0.0.0.0 --port 8000 --tensor-parallel-size 2 \
+ --gpu-memory-utilization 0.33 --max-model-len 32768 \
+ --served-model-name qwen2-vl
