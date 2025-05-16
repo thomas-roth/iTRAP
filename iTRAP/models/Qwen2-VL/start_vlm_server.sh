0a1df17507cfbbde0d89321e73881d9ef90e2484
thomas-roth <uruox@student.kit.edu>
2025-05-16 20:12:48 +0200

    update MoDE

--- iTRAP/models/Qwen2-VL/start_vlm_server.sh
+++ iTRAP/models/Qwen2-VL/start_vlm_server.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=4 python -m vllm.entrypoints.openai.api_server \
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server \
  --model /home/troth/bt/iTRAP/iTRAP/models/Qwen2-VL/pretrained/qwen2_vl_lora_sft_calvin \
  --host 0.0.0.0 --port 8000 \
  --gpu-memory-utilization 0.5 --max-model-len 32768 \
