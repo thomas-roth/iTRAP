40ab0573f0d782f4c8434f7bb37a26960641fd72
thomas-roth <uruox@student.kit.edu>
2025-07-08 18:02:26 +0200

    adjust config for HoreKa

--- iTRAP/models/Qwen2-VL/start_vlm_server.sh
+++ iTRAP/models/Qwen2-VL/start_vlm_server.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server \
- --model /home/troth/bt/iTRAP/iTRAP/models/Qwen2-VL/pretrained/qwen2_vl_lora_sft_calvin \
+CUDA_VISIBLE_DEVICES=4 python -m vllm.entrypoints.openai.api_server \
+ --model /home/hk-project-p0024638/uruox/code/iTRAP/iTRAP/models/Qwen2-VL/pretrained/qwen2_vl_lora_sft_calvin \
  --host 0.0.0.0 --port 8000 \
  --gpu-memory-utilization 0.5 --max-model-len 32768 \
  --served-model-name qwen2-vl
